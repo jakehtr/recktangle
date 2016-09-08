4580a77efcb34b48ecd54ad4f0d046cd288c0e09
Jake <jakehtr@gmail.com>
2016-09-08 01:36:49 +0100

    Variable name changes

--- recktangle.py
+++ recktangle.py
@@ -12,8 +12,8 @@ def divide(x, y):
 
 
 def recktangle(word, width, height):
-    left_right = list()
-    top_rows = list()
+    v_rows = list()
+    h_rows = list()
     final_reckt = str()
 
     if len(word) == 1:
@@ -21,14 +21,14 @@ def recktangle(word, width, height):
 
     # creating the vertical rows
     for j in range(len(word) - 2):
-        new_row = str()
-        new_row += '{0}{1}{2}{1}'.format(word[j + 1],
-                                         ' ' * (2 * len(word) - 3),
-                                         word[-2 - j]) * divide(width, 2)
+        v_row = str()
+        v_row += '{0}{1}{2}{1}'.format(word[j + 1],
+                                       ' ' * (2 * len(word) - 3),
+                                       word[-2 - j]) * divide(width, 2)
         if width % 2 == 0:
-            new_row += '{}'.format(word[j + 1])
+            v_row += '{}'.format(word[j + 1])
 
-        left_right.append('\n{}'.format(new_row.strip()))
+        v_rows.append('\n{}'.format(v_row.strip()))
 
     # creating the horizontal rows
     for i in range(2):
@@ -36,22 +36,22 @@ def recktangle(word, width, height):
             word_ = word
         else:
             word_ = word[::-1]
-        top = '{}'.format(word_[0])
+        h_row = '{}'.format(word_[0])
         for j in range(width):
             if j % 2 == 0:
-                top += ' {}'.format(' '.join(word_[1:]))
+                h_row += ' {}'.format(' '.join(word_[1:]))
             else:
-                top += ' {}'.format(' '.join(word_[:-1][::-1]))
-        top_rows.append(top)
+                h_row += ' {}'.format(' '.join(word_[:-1][::-1]))
+        h_rows.append(h_row)
 
     for k in range(height):
         if k % 2 == 0:
-            left_right_ = left_right
+            v_rows_ = v_rows
         else:
-            left_right_ = left_right[::-1]
+            v_rows_ = v_rows[::-1]
 
-        final_reckt += '{}{}\n'.format(top_rows[k % 2], ''.join(left_right_))
-    final_reckt += top_rows[height % 2]  # add bottom row
+        final_reckt += '{}{}\n'.format(h_rows[k % 2], ''.join(v_rows_))
+    final_reckt += h_rows[height % 2]  # add bottom row
 
     return '\n{}\n'.format(final_reckt)
 
